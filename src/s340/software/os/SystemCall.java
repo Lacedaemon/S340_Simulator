fce512d3a3e8b28a82bb8affeb2aa42673350bb0
Nathan Aragones <nathan@NathanscBookPro.lan1>
2018-10-29 16:26:30 -0700

    Added WRITE_CONSOLE SystemCall

--- src/s340/software/os/SystemCall.java
+++ src/s340/software/os/SystemCall.java
@@ -6,8 +6,9 @@ package s340.software.os;
 public interface SystemCall
 {
         public final static int SYSTEM_BREAK = 0;
+        public final static int WRITE_CONSOLE = 1;
         //public final static int SYSTEM_BREAK_MERGE = 2;
         //public final static int SYSTEM_BREAK_COMPACTIOM = 3;
     
-	public final static int NUM_SYSTEM_CALLS = 1;
+	public final static int NUM_SYSTEM_CALLS = 2;
 }
